8a3bc964e71a92b044c2e9064ae5b6d29fd65e0d
Jordan Hickling <jordan.hickling@netmatters-scs.com>
2022-08-25 14:30:06 +0100

    updated sidebar most recent update of: 25/08/2022

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,15 @@
 const hamburgerButton = document.querySelector('.hamburger-btn');
+const closeButton = document.querySelector('.close-btn');
 
 hamburgerButton.addEventListener("click", openNav)
 function openNav(){
-    document.getElementsByClassName("big-screen-sidebar")[0].style.width = "40vh";
-    document.getElementById("main").style.marginRight = "20%";
+    document.getElementsByClassName("big-screen-sidebar")[0].style.width = "22vw";
+    document.getElementById("main").style.marginRight = "20vw";
 }
 
 
 closeButton.addEventListener("click", closeNav)
 function closeNav(){
-    document.getElementsByClassName("sidebar-navigation")[0].style.width = "0";
+    document.getElementsByClassName("big-screen-sidebar")[0].style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
 }
