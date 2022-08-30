6ab48aaee21cf6dd3a9246fecfd241d7fa925f29
Jordan Hickling <jordan.hickling@netmatters-scs.com>
2022-08-30 15:48:23 +0100

    latest version 30/08/2022

--- JS/main.js
+++ JS/main.js
@@ -3,28 +3,71 @@ const closeButton = document.querySelector('.close-btn');
 
 hamburgerButton.addEventListener("click", openNav)
 function openNav(){
-    document.getElementsByClassName("big-screen-sidebar", "mobile-sidebar-navigation")[0].style.width = "24vw";
-    document.getElementById("main").style.marginRight = "22vw";
+    document.getElementsByClassName("sidebar")[0].style.width = "30%";
+    document.getElementById("main").style.marginRight = "25%";
+    document.getElementsByClassName("main-logo").style.marginRight = "25%";
 }
 
 
 closeButton.addEventListener("click", closeNav)
 function closeNav(){
-    document.getElementsByClassName("big-screen-sidebar", "mobile-sidebar-navigation")[0].style.width = "0";
+    document.getElementsByClassName("sidebar")[0].style.width = "0";
     document.getElementById("main").style.marginRight = "0";
 }
 
 
 
 
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 1){
-    $('sticky-header-nav').addClass('sticky-header');
-    }
-    else{
-    $('sticky-header-nav').removeClass('sticky-header');
-    }
-});
+// $(window).scroll(function() {
+//     if ($(this).scrollTop() > 1){
+//     $('sticky-header-nav').addClass('sticky-header');
+//     }
+//     else{
+//     $('sticky-header-nav').removeClass('sticky-header');
+//     }
+// });
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementsByClassName("sticky-header").style.top = "0";
+  } else {
+    document.getElementsByClassName("sticky-header").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+// let cookieModal = document.querySelector(".cookie-consent-modal")
+// let changeCookieBtn = document.querySelector(".btn.cancel")
+// let acceptCookieBtn = document.querySelector(".btn.accept")
+
+// changeCookieBtn.addEventListener("click", function (){
+//   cookieModal.classList.remove("active")
+// })
+
+// acceptCookieBtn.addEventListener("click", function (){
+//   cookieModal.classList.remove("active")
+//   localStorage.setItem("cookieAccepted", "yes")
+// })
+
+// setTimeout(function(){
+//   let cookieAccepted = localStorage.getItem("cookieAccepted")
+//   if (cookieAccepted != "yes"){
+//   cookieModal.classList.add("active")
+//   }
+// }, 2000)
+
+
+
+
+
+
+
+
+
+
 
 // window.onscroll = function() {stickyHeader()};
 
