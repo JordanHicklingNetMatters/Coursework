751503cbfd05078f3ba47cebc1c643bec9893df4
Jordan Hickling <jordan.hickling@netmatters-scs.com>
2022-10-31 15:23:13 +0000

    screen darkens on sidebar open, code templates for other functions that I need with description

--- JS/main.js
+++ JS/main.js
@@ -10,17 +10,61 @@ const closeButton = document.querySelector('.close-btn');
 
 //Open navigation on hamburger click. 
 hamburgerButton.addEventListener("click", openNav)
-function openNav(){
-    document.getElementsByClassName("sidebar")[0].style.width = "30%";
-    document.getElementById("main").style.marginRight = "25%";
+// Basic function to lock the scroll it currently locks the screen once then locks it completely once you exit then press it again it allows scrolling therefore this is not working. 
+function lockScroll() {
+    document.body.classList.toggle('lock-scroll');
 }
 
-//closing function for closebutton
+function openNav(){
+    document.getElementsByClassName("sidebar")[0].style.width = "30%";
+    document.getElementById("main").style.marginRight = "28%";
+    document.getElementsByClassName("sidebar")[0].style.boxShadow = "0 0 0 10000px rgba(0,0,0,.50)";
+}
+
+// closing function for closebutton
 closeButton.addEventListener("click", closeNav)
 function closeNav(){
     document.getElementsByClassName("sidebar")[0].style.width = "0";
     document.getElementById("main").style.marginRight = "0";
-}
+    document.getElementsByClassName("sidebar")[0].style.boxShadow = "0 0 0 10000px rgba(0,0,0,0)";
+} 
+
+// =====================================================================================================================================================================
+
+// Sidebar click outside body function. Resource below:
+// https://bobbyhadz.com/blog/javascript-hide-element-when-clicked-outside
+// Using this code I have tried to manipulate the tags and classes to those of mine and hopefully make it work however it did not. I need to look further into this. 
+
+// document.addEventListener('click', function handleClickOutsideSidebar(event) {
+//     // 👇️ the element the user clicked
+//     console.log('user clicked: ', event.target);
+  
+//     const sidebar = document.getElementsByClassName('sidebar');
+  
+//     if (!sidebar.contains(event.target)) {
+//       sidebar.style.display = 'none';
+//     }
+//   });
+
+// =====================================================================================================================================================================
+
+// $sidebar = $('sidebar');
+// $( "body" ).click(function( event ) {
+//   var a  = event.target;
+//   if(a === $sidebar){
+//     document.getElementsByClassName("sidebar")[0].style.width = "0";
+//     document.getElementById("main").style.marginRight = "0";
+//     document.getElementsByClassName("sidebar")[0].style.boxShadow = "0 0 0 10000px rgba(0,0,0,0)";
+//   }
+// });
+
+// JQuery to lock the mouse scroll when the Sidebar is open.
+// if ($('sidebar').is(':visible')) {
+//     $('body').addClass('lock-scroll');
+// } else {
+//     $('body').removeClass('lock-scroll');
+// }
+
 
 // $(document).click(function (e) {
  // function closeNav(){
@@ -83,7 +127,7 @@ function hasScrolled() {
     var st = $(this).scrollTop();
     
     // Make scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
+    if(Math.abs(lastScrollTop - st) <= delta) 
         return;
     
     // If scrolled down and past the navbar, add class .nav-up.
