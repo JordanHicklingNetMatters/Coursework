8edf501cf0e4c6fd5a9eaac7dcfb2096609d9f36
Jordan Hickling <jordan.hickling@netmatters-scs.com>
2022-11-16 10:59:54 +0000

    PHP fully indented working, sidebar has new php include page, JavaScript main file not commented out, data PHPADMIN has been changed to accept text type, testing on PHP has been complete

--- contact.php
+++ contact.php
@@ -12,129 +12,131 @@
     <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
 </head>
     <body>
-        <?php include("inc/header.php"); ?>
-        <div class="text-nav-container">
-            <h5 class="text-nav"><a href="index.php"> Home</a> / Our Offices</h5>
-        </div>
-        <div class="container">
-            <h3 id="office-header">Our Offices</h3>
-        </div>
-        <div class="flex-container">
-            <div class="location-container">
-                <img class="location-img" src="img/cambridge-office.jpg">
-                <div class="container">
-                    <h3 class="location-header"><a href="#">Cambridge Office</a></h3>
-                    <p class="location-details">
-                    Unit 1.31, <br>
-                    St John's Innovation Centre, <br>
-                    Cowley Road, Milton, <br>
-                    Cambridge, <br>
-                    CB4 0WS <br>
-                    </p>
-                    <h1 class="location-phone-num"><a href="tel:01223 37 57 72">01223 37 57 72</a></h1>
-                    <div class="view-more">
-                        <a href="#"><button class="view-more-button-tag" type="button">View More</button></a>
+        <div id="main">
+            <?php include("inc/header.php"); ?>
+            <div class="text-nav-container">
+                <h5 class="text-nav"><a href="index.php"> Home</a> / Our Offices</h5>
+            </div>
+            <div class="container">
+                <h3 id="office-header">Our Offices</h3>
+            </div>
+            <div class="flex-container">
+                <div class="location-container">
+                    <img class="location-img" src="img/cambridge-office.jpg">
+                    <div class="container">
+                        <h3 class="location-header"><a href="#">Cambridge Office</a></h3>
+                        <p class="location-details">
+                        Unit 1.31, <br>
+                        St John's Innovation Centre, <br>
+                        Cowley Road, Milton, <br>
+                        Cambridge, <br>
+                        CB4 0WS <br>
+                        </p>
+                        <h1 class="location-phone-num"><a href="tel:01223 37 57 72">01223 37 57 72</a></h1>
+                        <div class="view-more">
+                            <a href="#"><button class="view-more-button-tag" type="button">View More</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="location-container">
+                    <div class="img-container">
+                        <img class="location-img" src="img/wymondham-office.jpg">
+                    </div>
+                    <div class="container">
+                        <h3 class="location-header"><a href="#">Wymondham Office</a></h3>
+                        <p class="location-details">
+                        Unit 15, <br>
+                        Penfold Drive, <br>
+                        Gateway 11 Business Park, <br>
+                        Wymondham, Norfolk, <br>
+                        NR18 0WZ <br>
+                        </p>
+                        <h1 class="location-phone-num"><a href="tel:01603 70 40 20">01603 70 40 20</a></h1>
+                        <div class="view-more">
+                            <a href="#"><button class="view-more-button-tag" type="button">View More</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="location-container">
+                    <div class="img-container">
+                        <img class="location-img" src="img/yarmouth-office.jpg">
+                    </div>
+                    <div class="container">
+                        <h3 class="location-header"><a href="#">Great Yarmouth Office</a></h3>
+                        <p class="location-details">
+                        Suite F23, <br>
+                        Beacon Innovation Centre, <br>
+                        Beacon Park, Gorleston, <br>
+                        Great Yarmouth, Norfolk, <br>
+                        NR31 7RA <br>
+                        </p>
+                        <h1 class="location-phone-num"><a href="tel:01493 60 32 04">01493 60 32 04</a></h1>
+                        <div class="view-more">
+                            <a href="#"><button class="view-more-button-tag" type="button">View More</button></a>
+                            <!-- <a href="#" class="view-more-btn">View More</a> -->
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="location-container">
-                <div class="img-container">
-                    <img class="location-img" src="img/wymondham-office.jpg">
-                </div>
-                <div class="container">
-                    <h3 class="location-header"><a href="#">Wymondham Office</a></h3>
-                    <p class="location-details">
-                    Unit 15, <br>
-                    Penfold Drive, <br>
-                    Gateway 11 Business Park, <br>
-                    Wymondham, Norfolk, <br>
-                    NR18 0WZ <br>
-                    </p>
-                    <h1 class="location-phone-num"><a href="tel:01603 70 40 20">01603 70 40 20</a></h1>
-                    <div class="view-more">
-                        <a href="#"><button class="view-more-button-tag" type="button">View More</button></a>
+            <!-- <div class="flex-contact"> -->
+                <div class="form">
+                    <div class="container">
+                        <form id="contact-form" action="inc/contactForm.php" method="POST">
+                            <div class="form-group">
+                                <label for="name">Your Name <span class="required">*</span></label>
+                                <input class= "form-control" name="name" type="text" id="name">
+                                <p id="name-warning"><span class="required">*</span> You need to put in a name<p>
+                            </div>
+                            <div class="form-group">
+                                <label for="company-name">Company Name</label>
+                                <input class= "form-control" name="company-name" type="text" id="company-name">
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Your Email <span class="required">*</span></label>
+                                <input class= "form-control" name="email" type="email" id="email">
+                                <p id="email-warning"><span class="required">*</span> You need to put in an email<p>
+                                <p id="regex-warning"><span class="required">*</span> You need to put in a valid email<p>
+                            </div>
+                            <div class="form-group">
+                                <label for="telephone">Your Telephone Number <span class="required">*</span></label>
+                                <input class= "form-control" name="telephone" type="text" id="telephone">
+                                <p id="telephone-warning"><span class="required">*</span> You need to put in a telephone<p>
+                            </div>
+                            <div class="form-group">
+                                <label for="subject">Subject <span class="required">*</span></label>
+                                <input class= "form-control" name="subject" type="text" id="subject">
+                                <p id="subject-warning"><span class="required">*</span> You need to tell us the subject<p>
+                            </div>
+                                <label for="message">Message <span class="required">*</span></label>
+                                <textarea class="form-control" name="message" id="message"></textarea>
+                                <p id="message-warning"><span class="required">*</span> You must include a message!<p>
+                                <input class="tickbox" type="checkbox" id="accept" value="">
+                                <label class="accept-information" for="accept"> Please tick this box if you wish to receive
+                                    marketing information from us. Please see our <a href=#>Privacy Policy</a> for more information
+                                    on how we keep your data safe.
+                                </label>
+                            <div class="form-group">
+                                <button class="view-more-button-tag" type="submit" name="submit">SEND ENQURY</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
-            </div>
-            <div class="location-container">
-                <div class="img-container">
-                    <img class="location-img" src="img/yarmouth-office.jpg">
-                </div>
-                <div class="container">
-                    <h3 class="location-header"><a href="#">Great Yarmouth Office</a></h3>
-                    <p class="location-details">
-                    Suite F23, <br>
-                    Beacon Innovation Centre, <br>
-                    Beacon Park, Gorleston, <br>
-                    Great Yarmouth, Norfolk, <br>
-                    NR31 7RA <br>
-                    </p>
-                    <h1 class="location-phone-num"><a href="tel:01493 60 32 04">01493 60 32 04</a></h1>
-                    <div class="view-more">
-                        <a href="#"><button class="view-more-button-tag" type="button">View More</button></a>
-                        <!-- <a href="#" class="view-more-btn">View More</a> -->
+                    <div class="flex-container">
+                        <p><strong>Email us on:</strong></p>
+                        <br>
+                        <p><a href="#">sales@netmatters.com</a></p>
+                        <p><strong>Business hours:</strong></p>
+                        <br>
+                        <p><strong>Monday - Friday 07:00 - 18:00</strong></p>
                     </div>
-                </div>
-            </div>
+            <!-- </div> -->
+            <?php include("inc/newsletter.php"); ?>
+            <?php include("inc/footer.php"); ?>
+            <script src="js/jquery-3.6.0.min.js"></script>
+            <script src="https://kit.fontawesome.com/fc2f3a0d11.js" crossorigin="anonymous"></script>
+            <script src="js/contact.js"></script>
+            <script src="js/main.js"></script>
         </div>
-        <!-- <div class="flex-contact"> -->
-            <div class="form">
-                <div class="container">
-                    <form id="contact-form" action="inc/contactForm.php" method="POST">
-                        <div class="form-group">
-                            <label for="name">Your Name <span class="required">*</span></label>
-                            <input class= "form-control" name="name" type="text" id="name">
-                            <p id="name-warning"><span class="required">*</span> You need to put in a name<p>
-                        </div>
-                        <div class="form-group">
-                            <label for="company-name">Company Name</label>
-                            <input class= "form-control" name="company-name" type="text" id="company-name">
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Your Email <span class="required">*</span></label>
-                            <input class= "form-control" name="email" type="email" id="email">
-                            <p id="email-warning"><span class="required">*</span> You need to put in an email<p>
-                            <p id="regex-warning"><span class="required">*</span> You need to put in a valid email<p>
-                        </div>
-                        <div class="form-group">
-                            <label for="telephone">Your Telephone Number <span class="required">*</span></label>
-                            <input class= "form-control" name="telephone" type="text" id="telephone">
-                            <p id="telephone-warning"><span class="required">*</span> You need to put in a telephone<p>
-                        </div>
-                        <div class="form-group">
-                            <label for="subject">Subject <span class="required">*</span></label>
-                            <input class= "form-control" name="subject" type="text" id="subject">
-                            <p id="subject-warning"><span class="required">*</span> You need to tell us the subject<p>
-                        </div>
-                            <label for="message">Message <span class="required">*</span></label>
-                            <textarea class="form-control" name="message" id="message"></textarea>
-                            <p id="message-warning"><span class="required">*</span> You must include a message!<p>
-                            <input class="tickbox" type="checkbox" id="accept" value="">
-                            <label class="accept-information" for="accept"> Please tick this box if you wish to receive
-                                marketing information from us. Please see our <a href=#>Privacy Policy</a> for more information
-                                on how we keep your data safe.
-                            </label>
-                        <div class="form-group">
-                            <button class="view-more-button-tag" type="submit" name="submit">SEND ENQURY</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-                <div class="flex-container">
-                    <p><strong>Email us on:</strong></p>
-                    <br>
-                    <p><a href="#">sales@netmatters.com</a></p>
-                    <p><strong>Business hours:</strong></p>
-                    <br>
-                    <p><strong>Monday - Friday 07:00 - 18:00</strong></p>
-                </div>
-        <!-- </div> -->
-        <?php include("inc/newsletter.php"); ?>
-        <?php include("inc/footer.php"); ?>
-        <script src="js/jquery-3.6.0.min.js"></script>
-        <script src="https://kit.fontawesome.com/fc2f3a0d11.js" crossorigin="anonymous"></script>
-        <script src="js/contact.js"></script>
-        <!-- <script src="js/main.js"></script> -->
     </body>
 </html>
 
--- inc/newsArticles.php
+++ inc/newsArticles.php
@@ -1,104 +1,84 @@
 <?php
     include_once 'dbh.php';
 
-    // $title = $_GET['title'][1];
-    // $company = $_GET['imgsrc'][1];
-    // $description = $_GET['description'][1];
-    // $author = $_GET['author'][1];
-    // $authorsrc = $_GET['authorsrc'][1];
-    // $date = $_GET['dateposted'][1];
-
     $sql = "SELECT * FROM news WHERE id=1;";
     $result = $conn->query($sql);
-    // $resultCheck = mysqli_num_rows($articleOne);
 
 
+    echo '<div class="flex-container">';
 
-
-
-?>
-
-
-<div class="flex-container">
-    <div class="first-article">
-        <div class="news-content">
-            <?php
-            if ($result->rowCount() > 0)  { //checks the results if its 0 then it can run the code (prevents errors)
-                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    echo '<img class="main-article-img" src="' . $row['imgsrc'] . '" alt="A picture advertising a job for junior digital marketing executive.">';
-                    echo '<h4 class="h4-news">' . $row['title'] . '</h4>';
-                    echo '<p class="description">' . $row['description'] . '</p>';
-                    echo '<a href="#"><button class="news-read-more-btn" type="button">Read More</button></a>';
-                }
-            }
-            ?>
-            <div class="posted-by-section">
-                <div>
-                    <img class="avatar-img" src="img/news-avatar.png" alt="An avatar of the user who posted the article.">
-                </div>
-                <div class="details">
-                    <strong class="posted-strong">Posted by Netmatters</strong>
-                    <br>
-                    20th July 2022
-                </div>
-            </div>
-        </div>
-    </div><!-- This is the articals posted by page however when I input it, it just doesnt do anything so I will leave it commented for now. -->
-
-    <div class="second-article"> <!-- Second Artical -->
-        <div class="news-content">
-            <?php 
-                $secondSql = "SELECT * FROM news WHERE id=2;";
-                $secondResults = $conn->query($secondSql);
-            
-                if ($result->rowCount() > 0) { //checks the results if its 0 then it can run the code (prevents errors)
-                    while ($row = $secondResults->fetch(PDO::FETCH_ASSOC)) {
-                        echo '<img class="main-article-img" src="' . $row['imgsrc'] . '" alt="Text stating What you dont know about seo">';
+        if ($result->rowCount() > 0)  { //checks the results if its 0 then it can run the code (prevents errors)
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                echo '<div class="first-article">';
+                    echo '<div class="news-content">';
+                        echo '<img class="main-article-img" src="' . $row['imgsrc'] . '" alt="A picture advertising a job for junior digital marketing executive.">';
                         echo '<h4 class="h4-news">' . $row['title'] . '</h4>';
                         echo '<p class="description">' . $row['description'] . '</p>';
                         echo '<a href="#"><button class="news-read-more-btn" type="button">Read More</button></a>';
+                        echo '<div class="posted-by-section">';
+                            echo '<div>';
+                                echo '<img class="avatar-img" src="' . $row['authorsrc'] . '" alt="An avatar of the user who posted the article.">';
+                            echo '</div>';
+                            echo '<div class="details">';
+                                echo '<strong class="posted-strong">' . $row['author'] . '</strong><br>' . $row['dateposted'];
+                            echo '</div>';
+                        echo '</div>';
+                    echo '</div>';
+                echo '</div>';
+            }
+        }
+
+        $secondSql = "SELECT * FROM news WHERE id=2;";
+            $secondResults = $conn->query($secondSql);
+
+            if ($secondResults->rowCount() > 0) { //checks the results if its 0 then it can run the code (prevents errors)
+                while ($row = $secondResults->fetch(PDO::FETCH_ASSOC)) {
+                    echo '<div class="second-article">';
+                        echo '<div class="news-content">';
+                            echo '<img class="main-article-img" src="' . $row['imgsrc'] . '" alt="Text stating What you dont know about seo">';
+                            echo '<h4 class="h4-news">' . $row['title'] . '</h4>';
+                            echo '<p class="description">' . $row['description'] . '</p>';
+                            echo '<a href="#"><button class="news-read-more-btn" type="button">Read More</button></a>';
+                            echo '<div class="posted-by-section">';
+                                echo '<div>';
+                                    echo '<img class="avatar-img" src="' . $row['authorsrc'] . '" alt="An avatar of the user who posted the article.">';
+                                echo '</div>';
+                                echo '<div class="details">';
+                                    echo '<strong class="posted-strong">' . $row['author'] . '</strong><br>' . $row['dateposted'];
+                                echo '</div>';
+                            echo '</div>';
+                        echo '</div>';
+                    echo '</div>';
                     }
                 }
-            ?>
-            <div class="posted-by-section">
-                <div>
-                    <img class="avatar-img" src="img/news-avatar.png"
-                        alt="An avatar of the user who posted the article.">
-                </div>
-                <div class="details">
-                    <strong class="posted-strong">Posted by Netmatters</strong>
-                    <br>
-                    20th July 2022
-                </div>
-            </div> 
-        </div>
-    </div> <!-- Third Article -->
 
+        $thirdSql = "SELECT * FROM news WHERE id=3;";
+        $thirdResults = $conn->query($thirdSql);
 
-    <div class="hidden-card">
-        <div class="third-article">
-            <div class="news-content">
-                <img class="main-article-img" src="img/new-business-executive.jpg"
-                    alt="A picture with a phone advertising jobs for new business exectuives">
-                <h4 class="red-h4-news">New Business Executive</h4>
-                <p class="description">Salary Range £22,000 & OTE Hours 40 hours per week, Monday - Friday
-                    Location Wymondham, Norfolk/Part...</p>
-                <a href="#"><button class="red-news-read-more-btn" type="button">Read More</button></a>
-                <div class="posted-by-section">
-                    <div>
-                        <img class="avatar-img" src="img/news-avatar.png"
-                            alt="An avatar of the user who posted the article.">
-                    </div>
-                    <div class="details">
-                        <strong class="posted-strong">Posted by Netmatters</strong>
-                        <br>
-                        20th July 2022
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+        if ($thirdResults->rowCount() > 0) { //checks the results if its 0 then it can run the code (prevents errors)
+            while ($row = $thirdResults->fetch(PDO::FETCH_ASSOC)) {
+                echo '<div class="hidden-card">';
+                    echo '<div class="third-article">';
+                        echo '<div class="news-content">';
+                            echo '<img class="main-article-img" src="' . $row['imgsrc'] . '" alt="A picture with a phone advertising jobs for new business exectuives">';
+                            echo '<h4 class="red-h4-news">' . $row['title'] . '</h4>';
+                            echo '<p class="description">' . $row['description'] . '</p>';
+                            echo '<a href="#"><button class="red-news-read-more-btn" type="button">Read More</button></a>';
+                            echo '<div class="posted-by-section">';
+                                echo '<div>';
+                                    echo '<img class="avatar-img" src="' . $row['authorsrc'] . '" alt="An avatar of the user who posted the article.">';
+                                echo '</div>';
+                                echo '<div class="details">';
+                                    echo '<strong class="posted-strong">' . $row['author'] . '</strong><br>' . $row['dateposted'];
+                                echo '</div>';
+                            echo '</div>';
+                        echo '</div>';
+                    echo '</div>';
+                echo '</div>';
+                }
+            }
+    echo '</div>';
+?>
 <div class="view-all-section">
     <a class="view-link" href="#">View All <em>&rarr;</em></a>
 </div>
